6de5947e3eafe87f966a2508e1afff83a5e6674c
Anjali Prajapati <anjaliprajapati3122@gmail.com>
2021-10-27 12:59:12 +0400

    chat application

---
+++ client/src/store/reducer/process.js
@@ -0,0 +1,10 @@
+export const ProcessReducer = (state = {}, action) => {
+    switch (action.type) {
+      case "PROCESS":
+        return { ...action.payload };
+  
+      default:
+        return state;
+    }
+  };
+  
